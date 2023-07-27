cb1b75c52032a243b89905683a684cda2eb15892
Anhry <itech.capitals@icloud.com>
2023-07-27 22:20:24 +0300

    Update use-match-desktop.ts

--- components/clients-components/hooks/use-match-desktop.ts
+++ components/clients-components/hooks/use-match-desktop.ts
@@ -1,9 +1,9 @@
-import { useState, useLayoutEffect } from 'react'
+import { useState, useLayoutEffect, useEffect } from 'react'
 
 export const useMatchDesktop = () => {
     const [deskScreen, setDeskScreen] = useState(false)
 
-    useLayoutEffect(() => {
+    useEffect(() => {
         const checkMatches = () => {
             if (typeof window !== 'undefined') {
                 const mediaQuery = matchMedia('(max-width: 991px)')
@@ -26,3 +26,20 @@ export const useMatchDesktop = () => {
 
     return { isNotDesktop: deskScreen }
 }
+
+
+// import { useState, useLayoutEffect } from 'react'
+
+// const isDesktop = matchMedia('(max-width: 991px)')
+
+// export const useMatchDesktop = () => {
+//     const [deskScreen, setDeskScreen] = useState(isDesktop.matches)
+
+// useLayoutEffect(() => {
+//     const handler = () => setDeskScreen(isDesktop.matches)
+//     isDesktop.addEventListener('change', handler)
+//     return () => isDesktop.removeEventListener('change', handler)
+// })
+
+//     return { isNotDesktop: deskScreen }
+// }
