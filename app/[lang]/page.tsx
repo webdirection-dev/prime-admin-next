b0ba7075a9ed7a86ee9cdea1b2d0f5b2fb7839f4
Anhry <itech.capitals@icloud.com>
2023-07-31 21:00:30 +0300

    Update page.tsx

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -3,53 +3,37 @@ import { getDictionary } from '@/utils/language/dictionary'
 import TopPanel from '@/components/servers-components/layouts/dashboard/TopPanel'
 import { CardTitle } from '@/components/ui/CardTitle'
 import Card from '@/components/ui/withChildren/Card'
-import Test from '@/components/Test'
 
 export default async function Home({ params: { lang } }: { params: { lang: Locale } }) {
     const { tiles, btnTile }: any = await getDictionary(lang)
 
     return (
-        <h1>Test</h1>
+        <div className='grid mr-5'>
+            <TopPanel tile={tiles.tile} />
+
+            <article className='col-12 lg:col-6'>
+                <Card>
+                    <CardTitle title={tiles.recent.title} />
+                </Card>
+
+                <Card>
+                    <CardTitle title={tiles.best.title} btn={true} btnTile={btnTile} />
+                </Card>
+            </article>
+
+            <article className='col-12 lg:col-6'>
+                <Card>
+                    <CardTitle title={tiles.overview.title} />
+                </Card>
+
+                <Card>
+                    <CardTitle title={tiles.notifications.title} btn={true} btnTile={btnTile} />
+                </Card>
+
+                <Card>
+                    <CardTitle title={tiles.advertisement.title} />
+                </Card>
+            </article>
+        </div>
     )
 }
-
-
-// import { Locale } from '@/i18n.config'
-// import { getDictionary } from '@/utils/language/dictionary'
-// import TopPanel from '@/components/servers-components/layouts/dashboard/TopPanel'
-// import { CardTitle } from '@/components/ui/CardTitle'
-// import Card from '@/components/ui/withChildren/Card'
-
-// export default async function Home({ params: { lang } }: { params: { lang: Locale } }) {
-//     const { tiles, btnTile }: any = await getDictionary(lang)
-
-//     return (
-//         <div className='grid mr-5'>
-//             <TopPanel tile={tiles.tile} />
-
-//             <article className='col-12 lg:col-6'>
-//                 <Card>
-//                     <CardTitle title={tiles.recent.title} />
-//                 </Card>
-
-//                 <Card>
-//                     <CardTitle title={tiles.best.title} btn={true} btnTile={btnTile} />
-//                 </Card>
-//             </article>
-
-//             <article className='col-12 lg:col-6'>
-//                 <Card>
-//                     <CardTitle title={tiles.overview.title} />
-//                 </Card>
-
-//                 <Card>
-//                     <CardTitle title={tiles.notifications.title} btn={true} btnTile={btnTile} />
-//                 </Card>
-
-//                 <Card>
-//                     <CardTitle title={tiles.advertisement.title} />
-//                 </Card>
-//             </article>
-//         </div>
-//     )
-// }
