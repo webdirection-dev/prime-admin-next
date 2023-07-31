7102a15578108b23fb294cf2b0b946ccceb7552a
Anhry <itech.capitals@icloud.com>
2023-07-31 17:09:21 +0300

    up

--- components/servers-components/layouts/layout/General.tsx
+++ components/servers-components/layouts/layout/General.tsx
@@ -1,20 +1,32 @@
-import Header from "@/components/servers-components/layouts/layout/Header"
-import Footer from "@/components/servers-components/layouts/layout/Footer"
-import Navbar from '../../../clients-components/navbar/Navbar'
-import ContainerMain from '@/components/ui/withChildren/ContainerMain'
 import { Locale } from '@/i18n.config'
 
-export default function General({ children, lang }: { children: React.ReactNode, lang: Locale }) {
+export default function General({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <Header lang={lang} />
-            <main style={{ minHeight: 'calc(100vh - 7rem)' }} className='pt-8'>
-                <Navbar />
-                <ContainerMain>
-                    {children}
-                    <Footer />
-                </ContainerMain>
-            </main>
+            {children}
+
         </>
     )
 }
+
+
+// import Header from "@/components/servers-components/layouts/layout/Header"
+// import Footer from "@/components/servers-components/layouts/layout/Footer"
+// import Navbar from '../../../clients-components/navbar/Navbar'
+// import ContainerMain from '@/components/ui/withChildren/ContainerMain'
+// import { Locale } from '@/i18n.config'
+
+// export default function General({ children, lang }: { children: React.ReactNode, lang: Locale }) {
+//     return (
+//         <>
+//             <Header lang={lang} />
+//             <main style={{ minHeight: 'calc(100vh - 7rem)' }} className='pt-8'>
+//                 <Navbar />
+//                 <ContainerMain>
+//                     {children}
+//                     <Footer />
+//                 </ContainerMain>
+//             </main>
+//         </>
+//     )
+// }
