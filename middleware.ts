5d275c862b3a04ac7381e9656e49e999ef5e1fe1
Anhry <itech.capitals@icloud.com>
2023-07-31 17:43:17 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -48,6 +48,12 @@ function getLocale(request: NextRequest): string | undefined {
 export function middleware(request: NextRequest) {
     const pathname = request.nextUrl.pathname
 
+    // const lostFF = pathname.split('?')[0].endsWith('/ff')
+    // console.log(pathname.split('?'))
+    // console.log(pathname.split('?')[0])
+    // console.log(lostFF)
+
+
     const isTheme =
         pathname.endsWith('.svg') ||
         pathname.endsWith('/ff')
